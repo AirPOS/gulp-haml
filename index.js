60e080bcb9fb6185ce84de457b66dc323580ef8b
Gareth Andrew <gingerhendrix@gmail.com>
2014-05-06 16:18:41 +0100

    Work with hamlc (WIP)

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ module.exports = function(options) {
   if(!options) options = {};
   if(!options.ext) options.ext = '.html';
 
-  var htmlescape = function(input) {
-    input = input.replace('</script>', '</scr" + "ipt>');
-    input = JSON.stringify(input) + ';';
-    return input;
-  };
+  //var htmlescape = function(input) {
+    //input = input.replace('</script>', '</scr" + "ipt>');
+    //input = JSON.stringify(input) + ';';
+    //return input;
+  //};
 
-  var wrapIt = function(input, options) {
-      input = options.namespace + "['" + options.name + "'] = " + input;
-      input = '\n' + input + '\n';
-      return input;
-  };
+  //var wrapIt = function(input, options) {
+      //input = options.namespace + "['" + options.name + "'] = " + input;
+      //input = '\n' + input + '\n';
+      //return input;
+  //};
 
   // Map each file to this function
   function hamlStream(file, cb) {
@@ -25,7 +25,11 @@ module.exports = function(options) {
     if (file.isNull()) return cb(null, file); // pass along
     if (file.isStream()) return cb(new Error("gulp-hamlc: Streaming not supported"));
 
-    var output = hamlc.template(file.contents.toString("utf8"), path.basename(file.path, path.extname(file.path)), options.namespace, options)
+    var basename = path.dirname(path.relative(options.basedir, file.path)) + '/' + path.basename(file.path, '.jst.hamlc')
+    basename = basename.replace(/^\.\/(.*)/, "$1")
+    basename = basename.replace(/\//g, ".");
+
+    var output = hamlc.template(file.contents.toString("utf8"), basename, "window.JST", {extendScope: true})
 
     file.path = gutil.replaceExtension(file.path, options.ext);
     file.contents = new Buffer(output);
